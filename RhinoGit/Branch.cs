86af1195455665fb60b4d274e588ae41151d658f
amitlzkpa <amit.lzkpa@gmail.com>
2019-10-20 09:17:36 -0400

    add branch

---
+++ RhinoGit/Branch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Rhino;
+using Rhino.Commands;
+
+using LibGit2Sharp;
+
+namespace RhinoGit
+{
+   public class Branch : Command
+   {
+      static Branch _instance;
+      public Branch()
+      {
+         _instance = this;
+      }
+
+      ///<summary>The only instance of the Branch command.</summary>
+      public static Branch Instance
+      {
+         get { return _instance; }
+      }
+
+      public override string EnglishName
+      {
+         get { return "Branch"; }
+      }
+
+      protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+      {
+
+         string branchName = string.Empty;
+
+         try
+         {
+            Rhino.Input.Custom.GetString gs = new Rhino.Input.Custom.GetString();
+            gs.SetCommandPrompt("Source commit hash:");
+            gs.Get();
+            branchName = gs.StringResult();
+         }
+         catch (Exception ex)
+         {
+            RhinoApp.WriteLine(ex.ToString());
+            return Result.Failure;
+         }
+
+         if (string.IsNullOrEmpty(branchName)) return Result.Cancel;
+
+         using (RhinoGitCommand.repo)
+         {
+            RhinoGitCommand.repo.CreateBranch(branchName);
+         }
+
+         return Result.Success;
+      }
+   }
+}
