c672bfbcd6e033098e0c0c9a06aaf00a0c69e68c
amitlzkpa <amit.lzkpa@gmail.com>
2019-10-20 04:34:13 -0400

    remove probably wastelful code

--- RhinoGit/RGIndex.cs
+++ RhinoGit/RGIndex.cs
@@ -1,19 +1,14 @@
 ﻿using System;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
-using Rhino;
-using Rhino.Geometry;
 using Rhino.DocObjects;
 
 using Newtonsoft.Json.Serialization;
 
 namespace RhinoGit
 {
-   public class RGIndex : ISerializable
+   public class RGIndex
    {
       public SortedList items = new SortedList();
 
@@ -38,27 +33,5 @@ namespace RhinoGit
          return true;
       }
 
-      public bool RemoveByGeometry(GeometryBase geom)
-      {
-         Guid rid = Guid.Empty;
-         GeometryBase a = null;
-         foreach(RGItem ri in items)
-         {
-            //a = "convert to my geom";
-            if (GeometryBase.GeometryEquals(a, geom))
-            {
-               rid = ri.id;
-               break;
-            }
-         }
-         if (rid == Guid.Empty) return false;
-         RemoveById(rid);
-         return true;
-      }
-
-      public void GetObjectData(SerializationInfo info, StreamingContext context)
-      {
-
-      }
    }
 }
