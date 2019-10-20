acd1a631afb17aacdc0348c7e05b21bc2da536df
amitlzkpa <amit.lzkpa@gmail.com>
2019-10-20 08:10:03 -0400

    build show in compare

--- RhinoGit/Compare.cs
+++ RhinoGit/Compare.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 using Rhino;
 using Rhino.Commands;
+
+using LibGit2Sharp;
 
 namespace RhinoGit
 {
@@ -25,7 +32,42 @@ namespace RhinoGit
 
       protected override Result RunCommand(RhinoDoc doc, RunMode mode)
       {
-         // TODO: complete command.
+
+         string srcHash = string.Empty;
+
+         try
+         {
+            Rhino.Input.Custom.GetString gs = new Rhino.Input.Custom.GetString();
+            gs.SetCommandPrompt("Source commit hash:");
+            gs.Get();
+            srcHash = gs.StringResult();
+         }
+         catch (Exception ex)
+         {
+            RhinoApp.WriteLine(ex.ToString());
+            return Result.Failure;
+         }
+
+         if (string.IsNullOrEmpty(srcHash)) return Result.Cancel;
+
+         using (RhinoGitCommand.repo)
+         {
+            foreach(LibGit2Sharp.Commit cm in RhinoGitCommand.repo.Commits)
+            {
+               RhinoApp.WriteLine(cm.ToString());
+            }
+
+            var commit = RhinoGitCommand.repo.Commits.Single(c => c.Sha.ToString().StartsWith(srcHash.ToString()));
+            var file = commit[RhinoGitCommand.indexFileName];
+
+            var blob = file.Target as Blob;
+            using (var content = new StreamReader(blob.GetContentStream(), Encoding.UTF8))
+            {
+               var fileContent = content.ReadToEnd();
+               RhinoApp.WriteLine(fileContent);
+            }
+         }
+
          return Result.Success;
       }
    }
