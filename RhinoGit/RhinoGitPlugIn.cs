7443db87089a787602d94ef7e8e5f98be6869a1d
amitlzkpa <amit.lzkpa@gmail.com>
2019-10-20 03:12:42 -0400

    Add project files.

---
+++ RhinoGit/RhinoGitPlugIn.cs
@@ -0,0 +1,29 @@
+﻿namespace RhinoGit
+{
+   ///<summary>
+   /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+   /// class. DO NOT create instances of this class yourself. It is the
+   /// responsibility of Rhino to create an instance of this class.</para>
+   /// <para>To complete plug-in information, please also see all PlugInDescription
+   /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
+   /// "Show All Files" to see it in the "Solution Explorer" window).</para>
+   ///</summary>
+   public class RhinoGitPlugIn : Rhino.PlugIns.PlugIn
+
+   {
+      public RhinoGitPlugIn()
+      {
+         Instance = this;
+      }
+
+      ///<summary>Gets the only instance of the RhinoGitPlugIn plug-in.</summary>
+      public static RhinoGitPlugIn Instance
+      {
+         get; private set;
+      }
+
+      // You can override methods here to change the plug-in behavior on
+      // loading and shut down, add options pages to the Rhino _Option command
+      // and maintain plug-in wide options in a document.
+   }
+}
