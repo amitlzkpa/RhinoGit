8e6f37ff4c0b9f94b112df6ce52e2197a7486c1c
amitlzkpa <amit.lzkpa@gmail.com>
2019-10-20 03:28:25 -0400

    add item and index shell classes

---
+++ RhinoGit/RGIndex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Rhino;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+using Rhino.Commands;
+
+namespace RhinoGit
+{
+   public class RGIndex
+   {
+      List<RGItem> items = new List<RGItem>();
+
+      public RGIndex(IEnumerable<RhinoObject> rhos)
+      {
+         foreach(RhinoObject rho in rhos)
+         {
+            Add(rho);
+         }
+      }
+
+      public bool Add(RhinoObject rho)
+      {
+         return false;
+      }
+
+      public bool RemoveById(Guid id)
+      {
+         return false;
+      }
+
+      public bool RemoveByGeometry(GeometryBase geom)
+      {
+         return false;
+      }
+
+
+   }
+}
---
+++ RhinoGit/RGItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhinoGit
+{
+   class RGItem
+   {
+      public Guid id;
+      public string geometry;
+      public string attributes;
+
+      public bool EqualGeometry(RGIndex otherItem)
+      {
+         return false;
+      }
+
+      public bool EqualAttributes(RGIndex otherItem)
+      {
+         return false;
+      }
+
+   }
+}
